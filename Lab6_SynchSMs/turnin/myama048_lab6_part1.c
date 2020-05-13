cd10b0c53d534a06723573ad36d65dbd8e08e159
Masashi Yamaguchi <59752780+myama048@users.noreply.github.com>
2020-05-13 16:34:32 -0700

    Update myama048_lab6_part1.c

--- Lab6_SynchSMs/turnin/myama048_lab6_part1.c
+++ Lab6_SynchSMs/turnin/myama048_lab6_part1.c
@@ -1,7 +1,7 @@
-/*	Author: lab
+/*	Author: Masashi Yamaguchi
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #6  Exercise #
+ *	Lab Section:26
+ *	Assignment: Lab #6  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
