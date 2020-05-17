a3571a66717a14a29717d3374256c59d5bad79e9
myama048 <myama048@ucr.edu>
2020-05-17 03:12:55 -0700

    done

--- Lab8_ADC/turnin/myama048_lab8_part2.c
+++ Lab8_ADC/turnin/myama048_lab8_part2.c
@@ -1,7 +1,7 @@
 /*	Author: Masashi Yamaguchi
  *  Partner(s) Name: 
  *	Lab Section: 26
- *	Assignment: Lab #8  Exercise #3
+ *	Assignment: Lab #8  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -21,47 +21,19 @@ int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00;	PORTA = 0xFF;
 	DDRB = 0xFF;	PORTB = 0x00;
-	//DDRD = 0xFF;	PORTD = 0x00;
-
+	DDRD = 0xFF;	PORTD = 0x00;
 
 	unsigned short x;
-	//unsigned short y;
-	unsigned short z;
-	unsigned char tmpB = 0;
+	unsigned char tmpB, tmpD;
 	ADC_init();
-	const unsigned short MAX = 100;
-	const unsigned short MIN = 39;
     /* Insert your solution below */
     while (1) {
 	x = ADC;
-	//y = MAX / 2;
-	z = (MAX - MIN) / 8;
-	if(x > z){
-		tmpB = 1;
-	}
-	if(x > 2*z){
-		tmpB = 2|tmpB;
-	}
-	if(x > 3*z){
-		tmpB = 4|tmpB;
-	}
-	if(x > 4*z){
-		tmpB = 8|tmpB;
-	}
-	if(x > 5*z){
-		tmpB = 16|tmpB;
-	}
-	if(x > 6*z){
-		tmpB = 32|tmpB;
-	}
-	if(x > 7*z){
-		tmpB = 64|tmpB;
-	}
-	if(x > 8*z){
-		tmpB = 128|tmpB;
-	}
+	tmpB = (char)x;
+	tmpD = (char)(x >> 8);
 
 	PORTB = tmpB;
+	PORTD = tmpD;
     }
     return 1;
 }
--- Lab8_ADC/turnin/myama048_lab8_part3.c
+++ Lab8_ADC/turnin/myama048_lab8_part3.c
@@ -25,40 +25,20 @@ int main(void) {
 
 
 	unsigned short x;
-	//unsigned short y;
-	unsigned short z;
-	unsigned char tmpB = 0;
+	unsigned short y;
+	unsigned char tmpB;
 	ADC_init();
 	const unsigned short MAX = 100;
 	const unsigned short MIN = 39;
     /* Insert your solution below */
     while (1) {
 	x = ADC;
-	//y = MAX / 2;
-	z = (MAX - MIN) / 8;
-	if(x > z){
+	y = MAX / 2;
+	if(x >=  y){
 		tmpB = 1;
 	}
-	if(x > 2*z){
-		tmpB = 2|tmpB;
-	}
-	if(x > 3*z){
-		tmpB = 4|tmpB;
-	}
-	if(x > 4*z){
-		tmpB = 8|tmpB;
-	}
-	if(x > 5*z){
-		tmpB = 16|tmpB;
-	}
-	if(x > 6*z){
-		tmpB = 32|tmpB;
-	}
-	if(x > 7*z){
-		tmpB = 64|tmpB;
-	}
-	if(x > 8*z){
-		tmpB = 128|tmpB;
+	else {
+		tmpB = 0;
 	}
 
 	PORTB = tmpB;
